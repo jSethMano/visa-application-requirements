8a48bafaf6ca383e08db0e545df706a71d86c82c
Seth Maño <103634424+jSethMano@users.noreply.github.com>
2022-11-24 09:30:45 +0800

    Initial commit

--- src/service/APIService.js
+++ src/service/APIService.js
@@ -1,50 +1,43 @@
-import Axios from "axios";
+import Axios from 'axios'
 
-export async function apiCall(
-  url,
-  method,
-  data,
-  headers,
-  withCredentials = false,
-  params
-) {
-  let response;
-  try {
-    let resp = await Axios({
-      url,
-      method,
-      data,
-      headers,
-      withCredentials,
-      params
-    });
+export async function apiCall(url, method, data, headers, withCredentials = false, params) {
+	let response
+	try {
+		let resp = await Axios({
+			url,
+			method,
+			data,
+			headers,
+			withCredentials,
+			params,
+		})
 
-    if (resp) {
-      response = {
-        success: true,
-        status: resp.status,
-        data: resp.data
-      };
-    }
-  } catch (err) {
-    if (err.response) {
-      response = {
-        success: false,
-        data: err.response
-      };
-    } else if (err.request) {
-      response = {
-        success: false,
-        data: "Please check your connection.",
-        requestError: true
-      };
-    } else {
-      response = {
-        success: false,
-        data: err.message
-      };
-    }
-  }
+		if (resp) {
+			response = {
+				success: true,
+				status: resp.status,
+				data: resp.data,
+			}
+		}
+	} catch (err) {
+		if (err.response) {
+			response = {
+				success: false,
+				data: err.response,
+			}
+		} else if (err.request) {
+			response = {
+				success: false,
+				data: 'Please check your connection.',
+				requestError: true,
+			}
+		} else {
+			response = {
+				success: false,
+				data: err.message,
+			}
+		}
+	}
 
-  return response;
+	return response
 }
