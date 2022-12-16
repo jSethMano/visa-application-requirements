bb1a3a75676fd52e3e465ac69929343208c5d892
Seth Maño <103634424+jSethMano@users.noreply.github.com>
2022-12-16 17:01:49 +0800

    Refactor Master branch

--- src/query/get-application-by-id.js
+++ src/query/get-application-by-id.js
@@ -1,11 +1,6 @@
 import { apiCall } from '../service/APIService'
 
 export const getApplicationById = async (uId, token) => {
-	// let tokenStr = localStorage.getItem('visa-application-token')
-
-	// let tokenStr =
-	// 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjZlMWJiZmI2LTU3ODktNGIxZC1hYjg4LWM3NmI5M2JlMmU0NCIsImVtYWlsIjoiZ3Vlc3RAa21jLnNvbHV0aW9ucyIsInVuaXF1ZV9uYW1lIjoiZ3Vlc3RAa21jLnNvbHV0aW9ucyIsInJvbGUiOiJndWVzdCIsIm5iZiI6MTY3MTE3NzY1NSwiZXhwIjoxNjcxMjY0MDU1LCJpYXQiOjE2NzExNzc2NTV9.qtvtsTJmBVjNW2ZDZ5zA2B-BEsXSJ67iFdFZG_QW3Ro'
-
 	let webUrl = `${process.env.REACT_APP_ERP_ENDPOINT}/api/Visa/applications/${uId}`
 
 	let response = await apiCall(webUrl, 'GET', null, {
